4db3528ffcd4836fbbef7f0136548418d608b8bb
tsrsubbulaxmi@gmail.com <tsrsubbulaxmi@gmail.com>
2023-02-20 16:43:01 +0530

    initial commit

---
+++ CharArrayToStringExample.java
@@ -0,0 +1,13 @@
+public class CharArrayToStringExample  
+{  
+public static void main(String args[])  
+{  
+//character array  
+char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't'};  
+//constructor of the String class that parses char array as a parameter  
+String string = new String(ch);  
+//prints the string  
+System.out.println(string);  
+System.out.println("Hello World!!!");  
+}  
+}  
